5736fdc93ee9800ed37cbc5bb18124e3dfe9dc5f
Cameron Brown <hello@cameronbrown.co.uk>
2019-07-17 07:07:01 +0100

    added game scaffoloding

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, Response
+from requests import get
 
 
 app = Flask(__name__)
@@ -6,7 +7,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-  return 'Hello, world!'
+  return render_template('index.html')
+
+
+@app.route('/dist/build/bundle.css')
+def build_css():
+  """Request compiled CSS. This is only for development, and will be
+  overridden by the Google App Engine config.
+  
+  Local Webpack server must be running via `npm start`"""
+  return Response(get('http://localhost:8080/build/bundle.css').content, 
+                  mimetype='text/css')
+
+
+@app.route('/dist/build/bundle.js')
+def build_js():
+  """Request compiled JS. This is only for development, and will be
+  overridden by the Google App Engine config.
+  
+  Local Webpack server must be running via `npm start`"""
+  return Response(get('http://localhost:8080/build/bundle.js').content, 
+                  mimetype='application/javascript')
 
 
 if __name__ == '__main__':
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const autoprefixer = require('autoprefixer');
+// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+// const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: ['./src/scss/app.scss', './src/js/app.js'],
+  output: {
+    filename: 'build/bundle.js',
+  },
+  optimization: {
+    minimizer: [
+      //Incase you want to uglify/minify js
+      // new UglifyJsPlugin({
+      //   cache: true,
+      //   parallel: true,
+      //   sourceMap: true
+      // }),
+      // new OptimizeCSSAssetsPlugin({
+      //   cssProcessorOptions: { discardComments: { removeAll: true } },
+      //   canPrint: true
+      // })
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'build/bundle.css',
+            },
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()],
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['./node_modules'],
+            },
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        },
+      }
+    ],
+  },
+}
